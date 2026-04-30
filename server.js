5765a2ca766f9d8a2775b41d443ab818ff050761
eduardtreifeld <eduard.treifeld@gmail.com>
2026-04-30 12:42:53 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const MIME_TYPES = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.ico': 'image/x-icon'
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? '/VPM3.html' : req.url);
+  const ext = path.extname(filePath);
+  const contentType = MIME_TYPES[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`VPM server running on port ${PORT}`);
+});
